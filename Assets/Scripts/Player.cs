2478c01e273aa0da950a69ac3888df4e6e9b5fbf
YturAptoCul <louis.jacquin@etu.univ-smb.fr>
2022-06-24 14:43:37 +0200

    debut IA + point de vie

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private int pointDeVie;
+
+    public Text texteVie;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointDeVie = 100;
+        texteVie.text = $"Vie : {pointDeVie}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Ennemy":
+                pointDeVie -= 10;
+                break;
+            default:
+            Debug.Log("rien");
+            break;
+        }
+        texteVie.text = $"Vie : {pointDeVie}";
+    }
+}
---
+++ Assets/Scripts/ennemyCollision.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ennemyCollision : MonoBehaviour
+{
+    public NavMeshAgent myAgent;
+
+    public GameObject myTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myTarget != null)
+        {
+            myAgent.destination = myTarget.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            myTarget = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            myTarget = null;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            myTarget = null;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            myTarget = other.gameObject;
+        }
+    }
+}
